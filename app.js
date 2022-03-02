23789cdaab03f65d2c7bf433cb09a2434559102a
Inerizal <inerizal@gmail.com>
2022-03-02 12:57:19 +0300

    added details dipsplay and a way to chang params

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const field = document.querySelector('.field');
+const field = document.querySelector('#field');
+
 const startBtn = document.querySelector('#start');
 const stopBtn = document.querySelector('#stop');
 const restrtBtn = document.querySelector('#restart');
-const input = document.querySelector('#frequency');
 
-const width = 10;
+const sizeDetail = document.querySelector('#size');
+const maxCellsDetail = document.querySelector('#max-live');
+const cellsLeftDetail = document.querySelector('#cells-left');
+const speedDetail = document.querySelector('#speed');
+
+const setFrequency = document.querySelector('#set-frequency');
+const setSize = document.querySelector('#set-size');
+const setSeeds = document.querySelector('#set-seeds');
+
+
+
+
+
+let width = 100;
 
 let alive = 0;
 
-let cellNumber = 5;
+let cellNumber;
+let cellsLeft;
 
 let frequency = 5;
 
@@ -19,11 +33,17 @@ let nextGen = [];
 
 
 
-
 function createField() {
 
-    // field.style.gridTemplateColumns = `repeat(${width}, 1fr);`
+    field.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+
     cellNumber = 5;
+    cellsLeft = cellNumber;
+
+    sizeDetail.innerText = `${width} X ${width}`;
+    maxCellsDetail.innerText = `${cellNumber}`;
+    cellsLeftDetail.innerText = `${cellsLeft}`;
+    speedDetail.innerText = `${frequency}`;
 
 
     for(let i = 0; i < width*width; i++) {
@@ -55,72 +75,72 @@ function checkCells(time) {
         const isBottomEdge = (i >= width * width - width);
 
         if(i === 0) {
-            if(cells[cells.length - 1].value === 1) total++;
+            if(cells[cells.length - 1].cell.classList.contains('alive')) total++;
         }
         if(i === width - 1) {
-            if(cells[cells.length - width].value === 1) total++;
+            if(cells[cells.length - width].cell.classList.contains('alive')) total++;
         }
         if(i === cells.length - width) {
-            if(cells[width-1].value === 1) total++;
+            if(cells[width-1].cell.classList.contains('alive')) total++;
         }
         if(i === cells.length - 1) {
-            if(cells[0].value === 1) total++;
+            if(cells[0].cell.classList.contains('alive')) total++;
         }
 
         if(isLeftEdge) {
-            if(cells[i-1+width].value === 1) total++; // Left
-            if(!isTopEdge && cells[i-1].value === 1) total++; //Top Left
-            if(!isBottomEdge && cells[i-1+width*2].value === 1) total++; //Bottom Left
-            if(!isTopEdge && cells[i-width].value === 1) total++; //Top
-            if(!isBottomEdge && cells[i+width].value === 1) total++; //Bottom
-            if(!isTopEdge && cells[i+1-width].value === 1) total++; //Top Right
-            if(cells[i+1].value === 1) total++; //Right
-            if(!isBottomEdge && cells[i+1+width].value === 1) total++;//Bottom Right
+            if(cells[i-1+width].cell.classList.contains('alive')) total++; // Left
+            if(!isTopEdge && cells[i-1].cell.classList.contains('alive')) total++; //Top Left
+            if(!isBottomEdge && cells[i-1+width*2].cell.classList.contains('alive')) total++; //Bottom Left
+            if(!isTopEdge && cells[i-width].cell.classList.contains('alive')) total++; //Top
+            if(!isBottomEdge && cells[i+width].cell.classList.contains('alive')) total++; //Bottom
+            if(!isTopEdge && cells[i+1-width].cell.classList.contains('alive')) total++; //Top Right
+            if(cells[i+1].cell.classList.contains('alive')) total++; //Right
+            if(!isBottomEdge && cells[i+1+width].cell.classList.contains('alive')) total++;//Bottom Right
         }
 
         if(isTopEdge) {
-            if(!isLeftEdge && cells[i-1].value === 1) total++; // Left
-            if(!isLeftEdge && cells[i-1+(cells.length-width)].value === 1) total++; //Top Left
-            if(cells[i-1+width].value === 1) total++; //Bottom Left
-            if(cells[i+(cells.length-width)].value === 1) total++; //Top
-            if(cells[i+width].value === 1) total++; //Bottom
-            if(!isRightEdge && cells[i+1+(cells.length-width)].value === 1) total++; //Top Right
-            if(!isRightEdge && cells[i+1].value === 1) total++; //Right
-            if(!isRightEdge && cells[i+1+width].value === 1) total++;//Bottom Right
+            if(!isLeftEdge && cells[i-1].cell.classList.contains('alive')) total++; // Left
+            if(!isLeftEdge && cells[i-1+(cells.length-width)].cell.classList.contains('alive')) total++; //Top Left
+            if(cells[i-1+width].cell.classList.contains('alive')) total++; //Bottom Left
+            if(cells[i+(cells.length-width)].cell.classList.contains('alive')) total++; //Top
+            if(cells[i+width].cell.classList.contains('alive')) total++; //Bottom
+            if(!isRightEdge && cells[i+1+(cells.length-width)].cell.classList.contains('alive')) total++; //Top Right
+            if(!isRightEdge && cells[i+1].cell.classList.contains('alive')) total++; //Right
+            if(!isRightEdge && cells[i+1+width].cell.classList.contains('alive')) total++;//Bottom Right
         }
 
         if(isRightEdge) {
-            if(cells[i-1].value === 1) total++; // Left
-            if(!isTopEdge && cells[i-1-width].value === 1) total++; //Top Left
-            if(!isBottomEdge && cells[i-1+width].value === 1) total++; //Bottom Left
-            if(!isTopEdge && cells[i-width].value === 1) total++; //Top
-            if(!isBottomEdge && cells[i+width].value === 1) total++; //Bottom
-            if(!isTopEdge && cells[i+1-width*2].value === 1) total++; //Top Right
-            if(cells[i+1-width].value === 1) total++; //Right
-            if(!isBottomEdge && cells[i+1].value === 1) total++;//Bottom Right
+            if(cells[i-1].cell.classList.contains('alive')) total++; // Left
+            if(!isTopEdge && cells[i-1-width].cell.classList.contains('alive')) total++; //Top Left
+            if(!isBottomEdge && cells[i-1+width].cell.classList.contains('alive')) total++; //Bottom Left
+            if(!isTopEdge && cells[i-width].cell.classList.contains('alive')) total++; //Top
+            if(!isBottomEdge && cells[i+width].cell.classList.contains('alive')) total++; //Bottom
+            if(!isTopEdge && cells[i+1-width*2].cell.classList.contains('alive')) total++; //Top Right
+            if(cells[i+1-width].cell.classList.contains('alive')) total++; //Right
+            if(!isBottomEdge && cells[i+1].cell.classList.contains('alive')) total++;//Bottom Right
         }
 
         if(isBottomEdge) {
-            if(!isLeftEdge && cells[i-1].value === 1) total++; // Left
-            if(!isLeftEdge && cells[i-1-width].value === 1) total++; //Top Left
-            if(!isLeftEdge && cells[i-1-(cells.length-width)].value === 1) total++; //Bottom Left
-            if(cells[i-width].value === 1) total++; //Top
-            if(cells[i-(cells.length-width)].value === 1) total++; //Bottom
-            if(!isRightEdge && cells[i+1-width].value === 1) total++; //Top Right
-            if(!isRightEdge && cells[i+1].value === 1) total++; //Right
-            if(!isRightEdge && cells[i+1-(cells.length-width)].value === 1) total++;//Bottom Right
+            if(!isLeftEdge && cells[i-1].cell.classList.contains('alive')) total++; // Left
+            if(!isLeftEdge && cells[i-1-width].cell.classList.contains('alive')) total++; //Top Left
+            if(!isLeftEdge && cells[i-1-(cells.length-width)].cell.classList.contains('alive')) total++; //Bottom Left
+            if(cells[i-width].cell.classList.contains('alive')) total++; //Top
+            if(cells[i-(cells.length-width)].cell.classList.contains('alive')) total++; //Bottom
+            if(!isRightEdge && cells[i+1-width].cell.classList.contains('alive')) total++; //Top Right
+            if(!isRightEdge && cells[i+1].cell.classList.contains('alive')) total++; //Right
+            if(!isRightEdge && cells[i+1-(cells.length-width)].cell.classList.contains('alive')) total++;//Bottom Right
         }
 
         if(!isBottomEdge && !isLeftEdge && !isTopEdge && !isRightEdge) {
 
-            if(cells[i - 1].value === 1) total++; //Left
-            if(cells[i - 1 - width].value === 1) total++; //Top Left
-            if(cells[i - 1 + width].value === 1) total++; //Bottom Left
-            if(cells[i - width].value === 1) total++; //Top
-            if(cells[i + width].value === 1) total++; //Bottom
-            if(cells[i + 1].value === 1) total++; //Right
-            if(cells[i + 1 - width].value === 1) total++; //Top Right
-            if(cells[i + 1 + width].value === 1) total++; //Top Left
+            if(cells[i - 1].cell.classList.contains('alive')) total++; //Left
+            if(cells[i - 1 - width].cell.classList.contains('alive')) total++; //Top Left
+            if(cells[i - 1 + width].cell.classList.contains('alive')) total++; //Bottom Left
+            if(cells[i - width].cell.classList.contains('alive')) total++; //Top
+            if(cells[i + width].cell.classList.contains('alive')) total++; //Bottom
+            if(cells[i + 1].cell.classList.contains('alive')) total++; //Right
+            if(cells[i + 1 - width].cell.classList.contains('alive')) total++; //Top Right
+            if(cells[i + 1 + width].cell.classList.contains('alive')) total++; //Top Left
         }
 
 
@@ -149,33 +169,20 @@ function checkCells(time) {
 }
 
 function seedField(cell) {
-    if(cellNumber === 0) {
+    if(cellsLeft === 0) {
         cells.map(cell => cell.cell.removeEventListener('click', seedField));
     } else {
         cell.value = 1;
         cell.cell.classList.add('alive');
-        cellNumber--;
+        cellsLeft--;
+        cellsLeftDetail.innerText = `${cellsLeft}`;
+        cell.cell.removeEventListener('click', seedField)
     }
 }
 
 function initField() {
 
     cells.map(cell => cell.cell.addEventListener('click', () => seedField(cell)));
-
-    // cells[55].value = 1;
-    // cells[55].cell.classList.add('alive');
-
-    // cells[53].value = 1;
-    // cells[53].cell.classList.add('alive');
-
-    // cells[44].value = 1;
-    // cells[44].cell.classList.add('alive');
-
-    // cells[45].value = 1;
-    // cells[45].cell.classList.add('alive');
-
-    // cells[56].value = 1;
-    // cells[56].cell.classList.add('alive');
 }
 
 function clearField() {
@@ -196,9 +203,26 @@ function play() {
 
     initField();
 
-    input.addEventListener('change', () => {
-        frequency = input.value;
+    setFrequency.addEventListener('change', () => {
+        frequency = setFrequency.value;
         clearInterval(intervalId);
+    })
+    setSize.addEventListener('change', () => {
+        width = setSize.value;
+        clearInterval(intervalId);
+        intervalId = null;
+        clearField();      
+        createField();
+        initField();
+        
+    })
+    setSeeds.addEventListener('change', () => {
+        cellNumber = setSeeds.value;
+        cellsLeft = cellNumber;
+
+        maxCellsDetail.innerText = `${cellNumber}`;
+        cellsLeftDetail.innerText = `${cellsLeft}`;
+        
     })
 
     startBtn.addEventListener('click', () => {
@@ -208,11 +232,11 @@ function play() {
     stopBtn.addEventListener('click', () => clearInterval(intervalId));
 
     restrtBtn.addEventListener('click', () => {
+        console.log('click');
         clearInterval(intervalId);
         intervalId = null;
         clearField();      
         createField();
-        // console.log(cells);
         initField();
     })
 
